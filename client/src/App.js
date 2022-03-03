4601650351c684f5438e3dfe9cc898d6164316a5
thesahiljain <sahilj310@live.com>
2022-03-02 21:40:10 -0800

    march2

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,8 @@
-import Home from './Home/Home';
-import Navigation from './Navigation/Navigation';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
+
   return (
     <div className="App">
-      <Navigation/>
-      <Home/>
+      <h1>Hello</h1>
     </div>
   );
 }
